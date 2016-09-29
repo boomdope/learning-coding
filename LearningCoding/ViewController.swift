b501208b34790a8c8add30abf06dac47c963cc6f
BOOM <boom@BOOMs-MacBook-Pro.local>
2016-09-30 01:40:34 +0700

    Initial Commit

---
+++ LearningCoding/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LearningCoding
+//
+//  Created by BOOM on 9/30/2559 BE.
+//  Copyright © 2559 BOOM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
